96c99377cc30b225b6546c51f1659eb1459838b5
george <george@pop-os.localdomain>
2023-07-11 11:24:47 +0300

    Update on WeightingFunction

--- work/RapidSimAnalysis/WeightingFunction/Weighting.cpp
+++ work/RapidSimAnalysis/WeightingFunction/Weighting.cpp
@@ -9,7 +9,7 @@
 #include <chrono>
 #include <cmath>
 
-ROOT::RDF::RResultPtr<TH3D> normDistribution(std::string fileName, std::string treeName, std::string filter){
+ROOT::RDF::RResultPtr<TH3D> normDistribution(std::string fileName, std::string treeName){
         ROOT::RDataFrame dataFrame(treeName, fileName);
 
         int binNumber = 15;
@@ -25,10 +25,8 @@ ROOT::RDF::RResultPtr<TH3D> normDistribution(std::string fileName, std::string t
                 -3.5,
                 3.5
         };
-        auto hist = dataFrame.Filter(
-                filter
-        ).Histo3D(
-                {(fileName + filter).c_str(), "", binNumber, xRange[0], xRange[1], binNumber, yRange[0], yRange[1], binNumber, zRange[0], zRange[1]}, "D0_PT", "D0_eta","D0_phi"
+        auto hist = dataFrame.Histo3D(
+                {(fileName).c_str(), "", binNumber, xRange[0], xRange[1], binNumber, yRange[0], yRange[1], binNumber, zRange[0], zRange[1]}, "D0_PT", "D0_eta","D0_phi"
         );
         hist->Scale(1.0/hist->Integral());
         return hist;
@@ -46,36 +44,30 @@ int main(){
                 "../MomentumDependence/Dstp_D0__pimpip_pip_sPi_tree.root",
                 "../MomentumDependence/Dstp_D0__KmKp_pip_sPi_tree.root"
         };
-        std::vector<std::string> filters = {
-                "sPi_C == 1",
-                "sPi_C == -1"
-        };
+        // std::vector<std::string> filters = {
+        //         "sPi_C == 1",
+        //         "sPi_C == -1"
+        // };
         std::vector<ROOT::RDF::RResultPtr<TH3D>> distributions;
         std::vector<std::string> fileNamesNew = {
                 "Dstp_D0__KmKp_pip_sPi_weighted_tree.root",
                 "Dstp_D0__pimpip_pip_sPi_weighted_tree.root"
         };
         std::vector<std::string> plotNames = {
-                "Plots/KmKpPositive.pdf",
-                "Plots/KmKpNegative.pdf",
-                "Plots/pimpipPositive.pdf",
-                "Plots/pimpipNegative.pdf"
+                "Plots/KmKpNormalized.pdf",
+                "Plots/pimpipNormalized.pdf"
         };
         std::string treeName = "DecayTree";
 
         TCanvas* canvas = new TCanvas("canvas", "");
-        int k = 0;
         for (size_t i = 0; i < fileNames.size(); i++){
-                for (size_t j = 0; j < filters.size(); j++){
-                        distributions.push_back(normDistribution(fileNames[i], treeName, filters[j]));
-                        distributions[k]->SetStats(0);
-                        distributions[k]->GetXaxis()->SetTitle("P_{T} GeV/c");
-                        distributions[k]->GetYaxis()->SetTitle("#eta");
-                        distributions[k]->GetZaxis()->SetTitle("#phi");
-                        distributions[k]->Draw("LEGO2");
-                        canvas->SaveAs(plotNames[k].c_str());
-                        k += 1;
-                }
+                distributions.push_back(normDistribution(fileNames[i], treeName));
+                distributions[i]->SetStats(0);
+                distributions[i]->GetXaxis()->SetTitle("P_{T} GeV/c");
+                distributions[i]->GetYaxis()->SetTitle("#eta");
+                distributions[i]->GetZaxis()->SetTitle("#phi");
+                distributions[i]->Draw("LEGO2");
+                canvas->SaveAs(plotNames[i].c_str());
         }
 
 
